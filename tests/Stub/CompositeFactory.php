56d796d161dfe231f3fe28a1a17eb0b85a93e81a
Andrew Berry <deviantintegral@gmail.com>
2015-04-29 14:06:16 -0400

    Issue #2471829: Override the command factory when creating a client.

---
+++ tests/Stub/CompositeFactory.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Drupal\amazons3Test\Stub;
+
+use Drupal\amazons3Test\DrupalAdapter\Module;
+
+/**
+ * @class CompositeFactory
+ * @package Drupal\amazons3Test\Stub
+ */
+class CompositeFactory extends \Drupal\amazons3\CompositeFactory {
+  use Module;
+}
